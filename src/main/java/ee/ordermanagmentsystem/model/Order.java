a82c4c38890625c2263b548de3bafc96664dfd49
Stanislav M <ugresss@gmail.com>
2023-09-22 23:03:42 +0300

    init commit

--- src/main/java/ee/ordermanagmentsystem/model/Order.java
+++ src/main/java/ee/ordermanagmentsystem/model/Order.java
@@ -1,2 +1,33 @@
-package ee.ordermanagmentsystem.model;public class Order {
+package ee.ordermanagmentsystem.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Getter
+@Setter
+@SuperBuilder
+@NoArgsConstructor
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Customer customer;
+    private LocalDate submissionDate;
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderLine> orderLines = new ArrayList<>();
 }
--- src/main/java/ee/ordermanagmentsystem/model/OrderLine.java
+++ src/main/java/ee/ordermanagmentsystem/model/OrderLine.java
@@ -1,2 +1,35 @@
-package ee.ordermanagmentsystem.model;public class OrderLine {
+package ee.ordermanagmentsystem.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@SuperBuilder
+@NoArgsConstructor
+public class OrderLine {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    private Integer quantity;
 }
